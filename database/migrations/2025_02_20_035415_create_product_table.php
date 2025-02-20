3ca387780d407a98906ff0d6512ac0102cdd0adf
Alfonsius Tolan Hera <alfontolan4@gmail.com>
2025-02-20 11:00:21 +0700

    Create migration for Product table

---
+++ database/migrations/2025_02_20_035415_create_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary()->autoIncrement();
+            $table->unsignedBigInteger('seller_id');
+            $table->string('product_name', 20);
+            $table->integer('product_price');
+            $table->text('product_description');
+            $table->string('product_image', 255)->nullable();
+            $table->timestamps();
+
+            $table->foreign('seller_id')->references('id')->on('seller')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
