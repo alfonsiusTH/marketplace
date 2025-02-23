9055a167df3d2874849d91c3390d7df9cb3092a2
Fawasdiba <fawasabid@gmail.com>
2025-02-23 21:25:04 +0700

    feat: add login route and authentication controller mapping

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Authentication\AuthenticationController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('login', [AuthenticationController::class, 'showLogin'])->name('login');
