a4942772ba071e7e0d94fcc0da6c583c64159052
Fawasdiba <fawasabid@gmail.com>
2025-03-09 19:18:13 +0700

    feat:add authentication routes and dashboard access for sellers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Authentication\AuthenticationController;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SellerController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,5 +11,13 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('login', [AuthenticationController::class, 'showLogin'])->name('login');
+Route::get('/login', [AuthenticationController::class, 'showLogin'])->name('showLogin');
+Route::post('/login', [AuthenticationController::class, 'Login'])->name('login');
 Route::get('register', [AuthenticationController::class, 'showRegister'])->name('register');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard_toko', [ProductController::class, 'index'])->name('toko_dashboard');
+    Route::get('/detail_product/{id}', [ProductController::class, 'showDetail'])->name('detail_toko');
+
+    Route::get('/register_toko', [SellerController::class,  'create'])->name('register_toko');
+});
